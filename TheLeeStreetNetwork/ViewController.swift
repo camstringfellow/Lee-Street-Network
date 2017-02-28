cf19933238c4f31a7d5883a67ad5100a2be77460
camstringfellow <camstringfellow@gmail.com>
2017-02-28 16:39:21 -0500

    Completed email auth

--- TheLeeStreetNetwork/ViewController.swift
+++ TheLeeStreetNetwork/ViewController.swift
@@ -13,34 +13,9 @@ import Firebase
 
 class ViewController: UIViewController {
 
-    @IBAction func FBLogin(_ sender: Any) {
-    
-        let facebookLogin = FBSDKLoginManager()
-        
-        //Facebook Authenication
-        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
-            if error != nil {
-                
-                print("Unable to authenicate with Facebook - \(error)  ")
-                
-            } else if result?.isCancelled == true {
-                
-                print("User canceled Facebook login request")
-                
-            } else {
-                
-                print("Successfully authenicated with Facebook")
-                
-                //get credentail
-                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-                
-                self.firebaseAuth(credential)
-                
-            }//end if else
-            
-        } //facebookLogin.logIn
-        
-    }//FBLogin
+    @IBOutlet var emailField: FancyField!
+    //Dont forget to turn on privacy
+    @IBOutlet var passwordField: FancyField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,17 +27,91 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func firebaseAuth(_ credential: FIRAuthCredential) {
+    //EMAIL AUTHENTICATION ----------------------------------------------
+    
+    @IBAction func emailSigninButton(_ sender: AnyObject) {
         
-        //Authenicate with Firebase
-        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+        if let email = emailField.text, let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                
+                if error == nil {
+                    
+                    print("CAM: Email user successfully authenticated with Firebase")
+                    
+                } else {
+                    
+                    FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                      
+                        if error != nil {
+                            
+                            print("CAM: Unable to Authenticate with Firebase using email")
+                            print(error)
+                            
+                        } else {
+                            
+                            print("CAM: Successfully authenticated with Firebase")
+                            
+                        }
+                        
+                    })
+                    
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+    //-----------------------------------------------------------------
+    
+
+    //FACEBOOK AUTHENTICATION -----------------------------------------
+    
+    @IBAction func FBLogin(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
             if error != nil {
                 
-                print("Unable to authenicate with Firebase - \(error)  ")
+                print("CAM: Unable to authenicate with Facebook - \(error)  ")
+                
+            } else if result?.isCancelled == true {
+                
+                print("CAM: User canceled Facebook login request")
                 
             } else {
                 
-                print("Successfully authenicated with Firebase")
+                print("CAM: Successfully authenicated with Facebook")
+                
+                //get credentail
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+                
+            }//end if else
+            
+        } //facebookLogin.logIn
+        
+    }//FBLogin
+
+    //------------------------------------------------------------------
+    
+    
+    //FIREBASE AUTHENTICATION ------------------------------------------
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                
+                print("CAM: Unable to authenicate with Firebase - \(error)  ")
+                
+            } else {
+                
+                print("CAM: Successfully authenicated with Firebase")
                 
                 
             } //end else
@@ -70,6 +119,9 @@ class ViewController: UIViewController {
         }) //FIRAuth.auth()
         
     }
+    
+    //----------------------------------------------------------------------
+
     
 }
 
